6181cae1fccc9446a5934429efa8f14d4b76260e
唐诺 <caoweijian1991@163.com>
2017-09-29 15:53:06 +0800

    添加理解说明

--- app/src/main/java/com/example/caowj/newtask/example/activity/TestDaggerActivity.java
+++ app/src/main/java/com/example/caowj/newtask/example/activity/TestDaggerActivity.java
@@ -2,6 +2,7 @@ package com.example.caowj.newtask.example.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 import com.example.caowj.newtask.R;
 import com.example.caowj.newtask.example.mDragger.ApiService;
@@ -9,15 +10,24 @@ import com.example.caowj.newtask.example.mDragger.DaggerUserComponent;
 
 import javax.inject.Inject;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class TestDaggerActivity extends AppCompatActivity {
 
     @Inject
     ApiService apiService;
+    @BindView(R.id.tv_hint)
+    TextView tvHint;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_dragger);
+        ButterKnife.bind(this);
+
+        tvHint.setText("@Inject标识需要使用依赖\n@Modules标识提供依赖，内部通过@Provide具体操作\n@Component是连接的桥梁");
+
 
         //Dagger会自动创建这个类，以Dagger开头+UserComponent
         DaggerUserComponent.create().inject(this);
