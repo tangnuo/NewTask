1b21be2d39c35c7661d7048191c84c7e2da919dc
唐诺 <caoweijian1991@163.com>
2017-09-29 16:28:38 +0800

    优化代码

--- app/src/main/java/com/example/caowj/newtask/example/activity/TestDaggerActivity.java
+++ app/src/main/java/com/example/caowj/newtask/example/activity/TestDaggerActivity.java
@@ -14,7 +14,9 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 
 /**
- * 首次集成dagger2
+ * 首次集成dagger2<br/>
+ *
+ * http://www.cnblogs.com/pedro-neer/archive/2016/03/23/5309841.html
  */
 public class TestDaggerActivity extends BaseActivity {
 
@@ -28,11 +30,13 @@ public class TestDaggerActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         ButterKnife.bind(this);
 
-        tvHint.setText("@Inject标识需要使用依赖\n\r@Modules标识提供依赖，内部通过@Provide具体操作\n\r@Component是连接的桥梁");
+        tvHint.setText("@Inject标识需要使用依赖\n\n@Modules标识提供依赖，内部通过@Provide具体操作\n\n@Component是连接的桥梁");
 
 
         //Dagger会自动创建这个类，以Dagger开头+UserComponent
         DaggerUserComponent.create().inject(this);
+
+
         apiService.register(this);
     }
 
