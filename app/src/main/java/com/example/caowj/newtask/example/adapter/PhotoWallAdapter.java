d5cf1ddff5eb6cb4470fb94d9a0efc24efebd40f
唐诺 <caoweijian1991@163.com>
2017-11-22 11:11:38 +0800

    LruCache缓存配合本地缓存

--- app/src/main/java/com/example/caowj/newtask/example/adapter/PhotoWallAdapter.java
+++ app/src/main/java/com/example/caowj/newtask/example/adapter/PhotoWallAdapter.java
@@ -2,9 +2,8 @@ package com.example.caowj.newtask.example.adapter;
 
 import android.content.Context;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
-import android.support.v4.util.LruCache;
+import android.os.Environment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,10 +11,18 @@ import android.widget.AbsListView;
 import android.widget.ArrayAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.example.caowj.newtask.R;
 import com.example.caowj.newtask.example.data.Images;
+import com.example.caowj.newtask.utils.ImageLoader;
+import com.example.caowj.newtask.utils.LogUtil;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.HashSet;
@@ -23,6 +30,10 @@ import java.util.Set;
 
 /**
  * 下载图片后，存入LruCache中，显示图片时取值。
+ * 当LruCache中存储图片的总大小达到容量上限的时候，会自动把最近最少使用的图片从缓存中移除。
+ * <p>
+ * http://blog.csdn.net/guolin_blog/article/details/10470797
+ * </p>
  * package: com.example.caowj.newtask.example.adapter
  * author: Administrator
  * date: 2017/11/21 17:00
@@ -33,10 +44,7 @@ public class PhotoWallAdapter extends ArrayAdapter<String> implements AbsListVie
      */
     private Set<BitmapWorkerTask> taskCollection;
 
-    /**
-     * 图片缓存技术的核心类，用于缓存所有下载好的图片，在程序内存达到设定值时会将最少最近使用的图片移除掉。
-     */
-    private LruCache<String, Bitmap> mMemoryCache;
+    private ImageLoader imageLoader;
 
     /**
      * GridView的实例
@@ -57,22 +65,14 @@ public class PhotoWallAdapter extends ArrayAdapter<String> implements AbsListVie
      * 记录是否刚打开程序，用于解决进入程序不滚动屏幕，不会下载图片的问题。
      */
     private boolean isFirstEnter = true;
+    private int columnWidth = 120;
 
     public PhotoWallAdapter(Context context, int textViewResourceId, String[] objects,
                             GridView photoWall) {
         super(context, textViewResourceId, objects);
         mPhotoWall = photoWall;
         taskCollection = new HashSet<BitmapWorkerTask>();
-        // 获取应用程序最大可用内存
-        int maxMemory = (int) Runtime.getRuntime().maxMemory();
-        int cacheSize = maxMemory / 8;
-        // 设置图片缓存大小为程序最大可用内存的1/8
-        mMemoryCache = new LruCache<String, Bitmap>(cacheSize) {
-            @Override
-            protected int sizeOf(String key, Bitmap bitmap) {
-                return bitmap.getByteCount();
-            }
-        };
+        imageLoader = ImageLoader.getInstance();
         mPhotoWall.setOnScrollListener(this);
     }
 
@@ -100,41 +100,20 @@ public class PhotoWallAdapter extends ArrayAdapter<String> implements AbsListVie
      * @param imageView 用于显示图片的控件。
      */
     private void setImageView(String imageUrl, ImageView imageView) {
-        Bitmap bitmap = getBitmapFromMemoryCache(imageUrl);
+        Bitmap bitmap = imageLoader.getBitmapFromMemoryCache(imageUrl);
         if (bitmap != null) {
+            LogUtil.myD("00，从内存中获取");
             imageView.setImageBitmap(bitmap);
         } else {
             imageView.setImageResource(R.drawable.img_loading);
         }
     }
 
-    /**
-     * 将一张图片存储到LruCache中。
-     *
-     * @param key    LruCache的键，这里传入图片的URL地址。
-     * @param bitmap LruCache的键，这里传入从网络上下载的Bitmap对象。
-     */
-    public void addBitmapToMemoryCache(String key, Bitmap bitmap) {
-        if (getBitmapFromMemoryCache(key) == null) {
-            mMemoryCache.put(key, bitmap);
-        }
-    }
-
-    /**
-     * 从LruCache中获取一张图片，如果不存在就返回null。
-     *
-     * @param key LruCache的键，这里传入图片的URL地址。
-     * @return 对应传入键的Bitmap对象，或者null。
-     */
-    public Bitmap getBitmapFromMemoryCache(String key) {
-        return mMemoryCache.get(key);
-    }
-
     @Override
     public void onScrollStateChanged(AbsListView view, int scrollState) {
         // 仅当GridView静止时才去下载图片，GridView滑动时取消所有正在下载的任务
         if (scrollState == SCROLL_STATE_IDLE) {
-            loadBitmaps(mFirstVisibleItem, mVisibleItemCount);
+            showImageBitmap(mFirstVisibleItem, mVisibleItemCount);
         } else {
             cancelAllTasks();
         }
@@ -148,7 +127,7 @@ public class PhotoWallAdapter extends ArrayAdapter<String> implements AbsListVie
         // 下载的任务应该由onScrollStateChanged里调用，但首次进入程序时onScrollStateChanged并不会调用，
         // 因此在这里为首次进入程序开启下载任务。
         if (isFirstEnter && visibleItemCount > 0) {
-            loadBitmaps(firstVisibleItem, visibleItemCount);
+            showImageBitmap(firstVisibleItem, visibleItemCount);
             isFirstEnter = false;
         }
     }
@@ -160,16 +139,22 @@ public class PhotoWallAdapter extends ArrayAdapter<String> implements AbsListVie
      * @param firstVisibleItem 第一个可见的ImageView的下标
      * @param visibleItemCount 屏幕中总共可见的元素数
      */
-    private void loadBitmaps(int firstVisibleItem, int visibleItemCount) {
+    private void showImageBitmap(int firstVisibleItem, int visibleItemCount) {
+        if (!hasSDCard()) {
+            LogUtil.myD("未发现SD卡。。。");
+            return;
+        }
         try {
             for (int i = firstVisibleItem; i < firstVisibleItem + visibleItemCount; i++) {
                 String imageUrl = Images.imageThumbUrls[i];
-                Bitmap bitmap = getBitmapFromMemoryCache(imageUrl);
+                Bitmap bitmap = imageLoader.getBitmapFromMemoryCache(imageUrl);
                 if (bitmap == null) {
                     BitmapWorkerTask task = new BitmapWorkerTask();
                     taskCollection.add(task);
                     task.execute(imageUrl);
+                    LogUtil.myD("执行数据请求Task");
                 } else {
+                    LogUtil.myD("01，从内存中获取");
                     ImageView imageView = (ImageView) mPhotoWall.findViewWithTag(imageUrl);
                     if (imageView != null && bitmap != null) {
                         imageView.setImageBitmap(bitmap);
@@ -193,6 +178,16 @@ public class PhotoWallAdapter extends ArrayAdapter<String> implements AbsListVie
     }
 
     /**
+     * 判断手机是否有SD卡。
+     *
+     * @return 有SD卡返回true，没有返回false。
+     */
+    private boolean hasSDCard() {
+        return Environment.MEDIA_MOUNTED.equals(Environment
+                .getExternalStorageState());
+    }
+
+    /**
      * 异步下载图片的任务。
      *
      * @author guolin
@@ -202,56 +197,161 @@ public class PhotoWallAdapter extends ArrayAdapter<String> implements AbsListVie
         /**
          * 图片的URL地址
          */
-        private String imageUrl;
+        private String mImageUrl;
+
+        /**
+         * 可重复使用的ImageView
+         */
+        private ImageView mImageView;
+
+        public BitmapWorkerTask() {
+        }
 
         @Override
         protected Bitmap doInBackground(String... params) {
-            imageUrl = params[0];
-            // 在后台开始下载图片
-            Bitmap bitmap = downloadBitmap(params[0]);
-            if (bitmap != null) {
-                // 图片下载完成后缓存到LrcCache中
-                addBitmapToMemoryCache(params[0], bitmap);
+            //优先从内存中读取，然后是本地缓存，最后是网络获取。
+            mImageUrl = params[0];
+            Bitmap imageBitmap = imageLoader
+                    .getBitmapFromMemoryCache(mImageUrl);
+            if (imageBitmap == null) {
+                imageBitmap = getImageBitmap(mImageUrl);
+            } else {
+                LogUtil.myD("11，从内存中获取");
             }
-            return bitmap;
+            return imageBitmap;
         }
 
         @Override
         protected void onPostExecute(Bitmap bitmap) {
-            super.onPostExecute(bitmap);
-            // 根据Tag找到相应的ImageView控件，将下载好的图片显示出来。
-            ImageView imageView = (ImageView) mPhotoWall.findViewWithTag(imageUrl);
-            if (imageView != null && bitmap != null) {
-                imageView.setImageBitmap(bitmap);
+            if (bitmap != null) {
+                double ratio = bitmap.getWidth() / (columnWidth * 1.0);
+                int scaledHeight = (int) (bitmap.getHeight() / ratio);
+                addImage(bitmap, columnWidth, scaledHeight);
             }
             taskCollection.remove(this);
         }
 
         /**
-         * 建立HTTP请求，并获取Bitmap对象。
+         * 向ImageView中添加一张图片
+         *
+         * @param bitmap      待添加的图片
+         * @param imageWidth  图片的宽度
+         * @param imageHeight 图片的高度
+         */
+        private void addImage(Bitmap bitmap, int imageWidth, int imageHeight) {
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                    imageWidth, imageHeight);
+            // 根据Tag找到相应的ImageView控件，将下载好的图片显示出来。
+            mImageView = (ImageView) mPhotoWall.findViewWithTag(mImageUrl);
+            if (mImageView != null) {
+                mImageView.setImageBitmap(bitmap);
+            } else {
+                ImageView imageView = new ImageView(getContext());
+                imageView.setLayoutParams(params);
+                imageView.setImageBitmap(bitmap);
+                imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+                imageView.setPadding(5, 5, 5, 5);
+                imageView.setTag(mImageUrl);
+            }
+        }
+
+        /**
+         * 根据传入的URL，对图片进行加载。如果这张图片已经存在于SD卡中，则直接从SD卡里读取，否则就从网络上下载。
          *
          * @param imageUrl 图片的URL地址
-         * @return 解析后的Bitmap对象
+         * @return 加载到内存的图片。
          */
-        private Bitmap downloadBitmap(String imageUrl) {
-            Bitmap bitmap = null;
+        private Bitmap getImageBitmap(String imageUrl) {
+            File imageFile = new File(getImagePath(imageUrl));
+            if (!imageFile.exists()) {
+                downloadImage(imageUrl);
+            }
+            if (imageUrl != null) {
+                Bitmap bitmap = ImageLoader.decodeSampledBitmapFromResource(
+                        imageFile.getPath(), columnWidth);
+                LogUtil.myD("22，从本地存储中获取");
+                if (bitmap != null) {
+                    imageLoader.addBitmapToMemoryCache(imageUrl, bitmap);
+                    return bitmap;
+                }
+            }
+            return null;
+        }
+
+
+        /**
+         * 将图片下载到SD卡缓存起来。
+         *
+         * @param imageUrl 图片的URL地址。
+         */
+        private void downloadImage(String imageUrl) {
+            LogUtil.myD("33，从网络中获取");
             HttpURLConnection con = null;
+            FileOutputStream fos = null;
+            BufferedOutputStream bos = null;
+            BufferedInputStream bis = null;
+            File imageFile = null;
             try {
                 URL url = new URL(imageUrl);
                 con = (HttpURLConnection) url.openConnection();
                 con.setConnectTimeout(5 * 1000);
-                con.setReadTimeout(10 * 1000);
-                bitmap = BitmapFactory.decodeStream(con.getInputStream());
+                con.setReadTimeout(15 * 1000);
+                con.setDoInput(true);
+                con.setDoOutput(true);
+                bis = new BufferedInputStream(con.getInputStream());
+                imageFile = new File(getImagePath(imageUrl));
+                fos = new FileOutputStream(imageFile);
+                bos = new BufferedOutputStream(fos);
+                byte[] b = new byte[1024];
+                int length;
+                while ((length = bis.read(b)) != -1) {
+                    bos.write(b, 0, length);
+                    bos.flush();
+                }
             } catch (Exception e) {
                 e.printStackTrace();
+                LogUtil.myD("android6.0 以上系统，需要手动开启权限。");
             } finally {
-                if (con != null) {
-                    con.disconnect();
+                try {
+                    if (bis != null) {
+                        bis.close();
+                    }
+                    if (bos != null) {
+                        bos.close();
+                    }
+                    if (con != null) {
+                        con.disconnect();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
             }
-            return bitmap;
+            if (imageFile != null) {
+                Bitmap bitmap = ImageLoader.decodeSampledBitmapFromResource(
+                        imageFile.getPath(), columnWidth);
+                if (bitmap != null) {
+                    imageLoader.addBitmapToMemoryCache(imageUrl, bitmap);
+                }
+            }
         }
 
+        /**
+         * 获取图片的本地存储路径。
+         *
+         * @param imageUrl 图片的URL地址。
+         * @return 图片的本地存储路径。
+         */
+        private String getImagePath(String imageUrl) {
+            int lastSlashIndex = imageUrl.lastIndexOf("/");
+            String imageName = imageUrl.substring(lastSlashIndex + 1);
+            String imageDir = Environment.getExternalStorageDirectory()
+                    .getPath() + "/PhotoWallFalls/";
+            File file = new File(imageDir);
+            if (!file.exists()) {
+                file.mkdirs();
+            }
+            String imagePath = imageDir + imageName;
+            return imagePath;
+        }
     }
-
 }
