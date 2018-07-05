3ed21b8907896a50b2128fd87786fbc2f229a155
tangnuo <caowj1104@163.com>
2018-07-05 14:56:01 +0800

    学习约束布局

--- app/src/main/java/com/example/caowj/newtask/example/activity/ConstraintLayoutTestActivity.java
+++ app/src/main/java/com/example/caowj/newtask/example/activity/ConstraintLayoutTestActivity.java
@@ -6,7 +6,7 @@ import com.example.caowj.newtask.R;
 import com.kedacom.base.mvc.BaseButterKnifeActivity;
 
 /**
- * 约束布局
+ * 约束布局（可以使用不同的局部文件，查看不同的效果）
  */
 public class ConstraintLayoutTestActivity extends BaseButterKnifeActivity {
 
@@ -18,6 +18,7 @@ public class ConstraintLayoutTestActivity extends BaseButterKnifeActivity {
     @Override
     protected int getContentView() {
         return R.layout.activity_constraint_layout_test1;
+//        return R.layout.activity_constraint_layout_test2;
     }
 
     @Override
