89243977b6365bf0618e4a566fe9843a4555b485
caoweijian <caoweijian@kedacom.com>
2020-10-22 15:58:55 +0800

    kotlin重构代码；

--- module_main/src/main/java/com/kedacom/module_main/MainActivity.kt
+++ module_main/src/main/java/com/kedacom/module_main/MainActivity.kt
@@ -1,68 +1,55 @@
-package com.kedacom.module_main;
+package com.kedacom.module_main
 
-import android.content.Context;
-import android.content.Intent;
-import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.util.SparseArray;
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.util.SparseArray
+import com.kedacom.module_common.constant.RouteConstants
+import com.kedacom.module_common.service.ServiceFactory
+import com.kedacom.module_lib.base.common.BaseActivity
+import com.kedacom.module_lib.utils.LogUtil
+import com.kedacom.module_lib.utils.ToastUtil
+import com.kedacom.module_main.adapter.MainAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
-import com.kedacom.module_common.constant.RouteConstants;
-import com.kedacom.module_common.service.ILoginService;
-import com.kedacom.module_common.service.ServiceFactory;
-import com.kedacom.module_lib.base.common.BaseActivity;
-import com.kedacom.module_lib.utils.LogUtil;
-import com.kedacom.module_lib.utils.ToastUtil;
-import com.kedacom.module_main.adapter.MainAdapter;
+class MainActivity : BaseActivity() {
+    private var sparseArray: SparseArray<String>? = null
+    private var functionListAdapter: MainAdapter? = null
 
-public class MainActivity extends BaseActivity {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        LogUtil.myD("44444")
+        setContentView(R.layout.activity_main)
+        initData()
+        val accountService = ServiceFactory.getInstance().accountService
+        if (accountService.isLogin) {
+            LogUtil.myD("用户已登录" + accountService.accountId)
+        } else {
+            ToastUtil.showShortToast(mActivity, "请先登录！")
+        }
 
-    RecyclerView mRecyclerView;
+        mRecyclerView.layoutManager = LinearLayoutManager(mActivity)
+        mRecyclerView.adapter = functionListAdapter
+    }
 
-    private SparseArray<String> sparseArray;
-    private MainAdapter functionListAdapter;
+    private fun initData() {
+        sparseArray = SparseArray()
+        sparseArray!!.put(0, RouteConstants.LEARN_FUNCTION_LIST)
+        sparseArray!!.put(1, RouteConstants.NEWS_ACTIVITY)
+        functionListAdapter = MainAdapter(mActivity, sparseArray!!)
+    }
 
-    public static void newInstance(Context context) {
-        //显示Intent
+    companion object {
+        fun newInstance(context: Context) {
+            //显示Intent
 //        Intent intent = new Intent(context, FunctionListActivity.class);
 //        context.startActivity(intent);
 
 //        隐式Intent
-        String mAction = context.getPackageName() + ".FunctionList";
-        Intent intent = new Intent(mAction);
-        context.startActivity(intent);
-    }
-
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        LogUtil.myD("44444");
-
-        setContentView(R.layout.activity_main);
-
-        initData();
-
-
-        ILoginService accountService = ServiceFactory.getInstance().getAccountService();
-
-        if (accountService.isLogin()) {
-            LogUtil.myD("用户已登录" + accountService.getAccountId());
-        } else {
-            ToastUtil.showShortToast(mActivity, "请先登录！");
+            val mAction = context.packageName + ".FunctionList"
+            val intent = Intent(mAction)
+            context.startActivity(intent)
         }
-        mRecyclerView = findViewById(R.id.mRecyclerView);
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(mActivity));
-        mRecyclerView.setAdapter(functionListAdapter);
-    }
-
-
-    protected void initData() {
-        sparseArray = new SparseArray<>();
-
-        sparseArray.put(0, RouteConstants.LEARN_FUNCTION_LIST);
-        sparseArray.put(1, RouteConstants.NEWS_ACTIVITY);
-
-        functionListAdapter = new MainAdapter(mActivity, sparseArray);
     }
 }
--- module_main/src/main/java/com/kedacom/module_main/adapter/MainAdapter.kt
+++ module_main/src/main/java/com/kedacom/module_main/adapter/MainAdapter.kt
@@ -1,108 +1,83 @@
-package com.kedacom.module_main.adapter;
+package com.kedacom.module_main.adapter
 
-import android.content.Context;
-import android.graphics.Color;
-import android.support.v7.widget.RecyclerView;
-import android.util.SparseArray;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
-
-import com.alibaba.android.arouter.facade.Postcard;
-import com.alibaba.android.arouter.facade.callback.NavCallback;
-import com.alibaba.android.arouter.launcher.ARouter;
-import com.kedacom.module_common.bean.main.UserInfo;
-import com.kedacom.module_lib.utils.LogUtil;
-import com.kedacom.module_main.R;
+import android.content.Context
+import android.graphics.Color
+import android.support.v7.widget.RecyclerView
+import android.util.SparseArray
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.alibaba.android.arouter.facade.Postcard
+import com.alibaba.android.arouter.facade.callback.NavCallback
+import com.alibaba.android.arouter.launcher.ARouter
+import com.kedacom.module_common.bean.main.UserInfo
+import com.kedacom.module_lib.utils.LogUtil
+import com.kedacom.module_main.R
 
 /**
  * @Dec ：
  * @Author : Caowj
  * @Date : 2018/8/22 13:13
  */
-public class MainAdapter extends RecyclerView.Adapter {
+class MainAdapter(private val mContext: Context, sparseArray1: SparseArray<String>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     //    private List<String> mList = new ArrayList<>();
-    private SparseArray<String> sparseArray;
-    private Context mContext;
-    private LayoutInflater mLayoutInflater;
+    private var sparseArray: SparseArray<String> = sparseArray1
+    private val mLayoutInflater: LayoutInflater = LayoutInflater.from(mContext)
 
-    public MainAdapter(Context mContext, SparseArray<String> sparseArray1) {
-        this.mContext = mContext;
-        sparseArray = sparseArray1 == null ? new SparseArray<String>() : sparseArray1;
-        mLayoutInflater = LayoutInflater.from(mContext);
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return FunctionViewHolder(mLayoutInflater.inflate(R.layout.item_todo, parent, false))
     }
 
-    @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return new FunctionViewHolder(mLayoutInflater.inflate(R.layout.item_todo, parent, false));
-    }
-
-    @Override
-    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-        String routePath = sparseArray.get(position);
-        FunctionViewHolder fHolder = (FunctionViewHolder) holder;
-        fHolder.tvTitle.setText(routePath);
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val routePath = sparseArray[position]
+        val fHolder = holder as FunctionViewHolder?
+        fHolder!!.tvTitle.text = routePath
         if (position % 3 == 0) {
-            fHolder.itemView.setBackgroundColor(Color.parseColor("#fa8072"));
+            fHolder.itemView.setBackgroundColor(Color.parseColor("#fa8072"))
         } else {
-            fHolder.itemView.setBackgroundColor(Color.parseColor("#354825"));
+            fHolder.itemView.setBackgroundColor(Color.parseColor("#354825"))
         }
-
-        fHolder.itemView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                UserInfo userInfo = new UserInfo(12, "caowj", "留园");
-
-                if (position == 0) {
-                    ARouter.getInstance().build(routePath)
-                            .withString("name", "曹维健")
-                            .withLong("age", 25)
-                            .withParcelable("userInfo", userInfo)
-                            .navigation();
-                } else {
-                    //简单写法：
+        fHolder.itemView.setOnClickListener {
+            val userInfo = UserInfo(12, "caowj", "留园")
+            if (position == 0) {
+                ARouter.getInstance().build(routePath)
+                        .withString("name", "曹维健")
+                        .withLong("age", 25)
+                        .withParcelable("userInfo", userInfo)
+                        .navigation()
+            } else {
+                //简单写法：
 //                    ARouter.getInstance().build(routePath).navigation();
 
-                    //监听过程：
-                    ARouter.getInstance().build(routePath).navigation(mContext, new NavCallback() {
+                //监听过程：
+                ARouter.getInstance().build(routePath).navigation(mContext, object : NavCallback() {
+                    override fun onFound(postcard: Postcard) {
+                        LogUtil.myD("onFound: 找到了 ")
+                    }
 
-                        @Override
-                        public void onFound(Postcard postcard) {
-                            LogUtil.myD("onFound: 找到了 ");
-                        }
+                    override fun onLost(postcard: Postcard) {
+                        LogUtil.myD("onLost: 找不到了 ")
+                    }
 
-                        @Override
-                        public void onLost(Postcard postcard) {
-                            LogUtil.myD("onLost: 找不到了 ");
-                        }
+                    override fun onArrival(postcard: Postcard) {
+                        LogUtil.myD("onArrival: 跳转完了 ")
+                    }
 
-                        @Override
-                        public void onArrival(Postcard postcard) {
-                            LogUtil.myD("onArrival: 跳转完了 ");
-                        }
-
-                        @Override
-                        public void onInterrupt(Postcard postcard) {
-                            LogUtil.myD("onInterrupt: 被拦截了 ");
-                        }
-                    });
-                }
+                    override fun onInterrupt(postcard: Postcard) {
+                        LogUtil.myD("onInterrupt: 被拦截了 ")
+                    }
+                })
             }
-        });
-    }
-
-    @Override
-    public int getItemCount() {
-        return sparseArray.size();
-    }
-
-    class FunctionViewHolder extends RecyclerView.ViewHolder {
-        TextView tvTitle;
-
-        public FunctionViewHolder(View view) {
-            super(view);
-            tvTitle = view.findViewById(R.id.textView);
         }
     }
+
+    override fun getItemCount(): Int {
+        return sparseArray.size()
+    }
+
+    internal inner class FunctionViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        var tvTitle: TextView = view.findViewById(R.id.textView)
+    }
+
 }
