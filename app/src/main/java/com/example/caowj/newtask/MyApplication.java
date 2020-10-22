3a07db8d7d799f8be9e7e4acf22769459b727bdb
caoweijian <caoweijian@kedacom.com>
2020-10-22 15:25:26 +0800

    优化配置；

--- app/src/main/java/com/example/caowj/newtask/MyApplication.java
+++ app/src/main/java/com/example/caowj/newtask/MyApplication.java
@@ -10,14 +10,12 @@ import com.kedacom.module_lib.base.common.BaseApplication;
  */
 public class MyApplication extends BaseApplication {
 
-    private boolean isDebug = true;
-
     @Override
     public void onCreate() {
         super.onCreate();
         //因为只有在集成模式下才会用到ARouter，所以放在APP—module中。
 
-        if (isDebug) {
+        if (BuildConfig.isARouter) {
             //开启InstantRun之后，一定要在ARouter.init之前调用openDebug
             ARouter.openDebug();
             ARouter.openLog();
